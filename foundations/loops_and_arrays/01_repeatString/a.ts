d74bd726f17eaaaf0694150076501d15aee38434
enochhayford1 <e.hayford@lancaster.ac.uk>
2026-09-15 17:37:34 +0100

    All tests passed

---
+++ foundations/loops_and_arrays/01_repeatString/a.ts
@@ -0,0 +1,3 @@
+function max(a: number, b: number) : number {
+    return (a > b) ? a: b;
+}
--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return `ERROR`;
 
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push(string);
+    }
+    return arr.join(``);
 };
 
 // Do not edit below this line
