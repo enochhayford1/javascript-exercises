6b27f318f6cb2e32dc8aeee17fb7ec35e995b572
enochhayford1 <e.hayford@lancaster.ac.uk>
2026-09-16 11:06:45 +0100

    All tests passed

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let result = [];
+    for (let item of arr) {
+        if (!args.includes(item)) {
+            result.push(item);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
