e80dc283fea8aa9568cc2b30208f563e184d06e3
enochhayford1 <e.hayford@lancaster.ac.uk>
2026-09-16 10:53:45 +0100

    All tests passed

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let chars = [];
+    for (let char of str) {
+        chars.push(char);
+    }
+    const reversed = chars.toReversed();
+    return reversed.join(``);
 };
 
 // Do not edit below this line
