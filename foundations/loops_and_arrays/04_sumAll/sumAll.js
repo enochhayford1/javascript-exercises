b4e5dcb092e4aa9ff0eb7208412c3e76e761ea4c
enochhayford1 <e.hayford@lancaster.ac.uk>
2026-09-16 11:22:07 +0100

    All tests passed

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if ((isNaN(a) || a < 0 || !Number.isInteger(a)) || (isNaN(b) || b < 0 || !Number.isInteger(b))) {
+        return `ERROR`;
+    }
 
+    let smaller = 0;
+    let larger = 0;
+
+    if (a < b) {
+        smaller = a;
+        larger = b;
+    } else if (b < a) {
+        smaller = b;
+        larger = a;
+    }
+
+
+    let sum = 0;
+    for (let i = smaller; i <= larger; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
