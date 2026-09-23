0c1df547f8a268c28a17c1279b5daf31cdeb10fa
enochhayford1 <e.hayford@lancaster.ac.uk>
2026-09-23 11:25:59 +0100

    All tests passed

--- foundations/object_basics/01_calculator/calculator.js
+++ foundations/object_basics/01_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (arr.length == 0) return 0;
+  return arr.reduce((total, current) => total + current)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) return 1;
+  
+  let result = num;
+
+  for (let i = num-1; i > 1; i--) {
+    result = result * i; 
+  }
+  return result;
 };
 
 // Do not edit below this line
